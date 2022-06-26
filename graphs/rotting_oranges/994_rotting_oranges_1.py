5e8c55f537f728cf6e0756c5a4ff540b1b2e8586
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-06-26 15:53:00 -0700

    Add Rotting Oranges (LC994) Solution

--- graphs/rotting_oranges/994_rotting_oranges_1.py
+++ graphs/rotting_oranges/994_rotting_oranges_1.py
@@ -1,1 +1,57 @@
-#type: ignore
+from typing import List
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        """Use BFS while rot can be spread to fresh oranges.  Uses (row, col) order."""
+        minutes = 0
+        fresh = 0
+        # Use for bounds checks
+        last_row, last_col = len(grid) - 1, len(grid[0]) - 1
+        # Stores (row, col, turn rotted) of rotten oranges
+        rotten = deque()
+        explored = set()
+        for row, tiles in enumerate(grid):
+            for col, tile in enumerate(tiles):
+                if tile == 2:
+                    rotten.append((row, col, 0))
+                    explored.add((row, col))
+                elif tile == 1:
+                    fresh += 1
+        # Collect all rotten and propagate rot until no rotten remain
+        while rotten:
+            # Get rotten orange and update time elapsed
+            (row, col, turn_rotted) = rotten.popleft()
+            minutes = max(minutes, turn_rotted)
+            # Check neighbors for fresh oranges and convert them w/new rot turn
+            north = (row-1, col)
+            south = (row+1, col)
+            west  = (row, col-1)
+            east  = (row, col+1)
+            if row > 0 and north not in explored:
+                explored.add(north)
+                if grid[north[0]][north[1]] == 1:
+                    rotten.append((*north, turn_rotted+1))
+                    fresh -= 1
+            if row < last_row and south not in explored:
+                explored.add(south)
+                if grid[south[0]][south[1]] == 1:
+                    rotten.append((*south, turn_rotted+1))
+                    fresh -= 1
+            if col > 0 and west not in explored:
+                explored.add(west)
+                if grid[west[0]][west[1]] == 1:
+                    rotten.append((*west, turn_rotted+1))
+                    fresh -= 1
+            if col < last_col and east not in explored:
+                explored.add(east)
+                if grid[east[0]][east[1]] == 1:
+                    rotten.append((*east, turn_rotted+1))
+                    fresh -= 1
+
+        # If all rot has spread and fresh oranges remain -> fail.
+        if fresh > 0:
+            return -1
+        else:
+            return minutes
+            
--- graphs/rotting_oranges/README.md
+++ graphs/rotting_oranges/README.md
@@ -21,11 +21,33 @@ Constraints:
 ## Thought Process
 
 Edge Cases / Caveats / Pitfalls:
+- Cases where rot can't spread to a fresh orange
+- Case where all oranges are rotten
+- Account for cases w/no oranges
+- Account for cases where neighboring orange is already rotten
+- Properly getting the *minimum* number of *turns*
+- How to count `minutes`
+
+Game Plan:
+- Track all `fresh` and `rotten` oranges at start
+- For every `rotten` orange, spread `rotten` status to any `fresh` orange
+- Increment `time` counter *if* at least one orange was converted
+- Continue until all oranges are `rotten` or until a turn passes when no fresh orange was converted (unreachable).
 
 ## Procedure
 
-### Method 1
+### Method 1: Breadth-First Search w/Generation Tracking
+
+Big Picture:
+1. Store count of `fresh` oranges and `minutes` elapsed (starting at `0`)
+2. Place all `rotten` oranges in a queue with `(row, col, turn_rotted)`
+3. Perform BFS with all `rotten` oranges:
+    - update `minutes` with the highest of itself and `turn_rotted`
+    - spread rot to any `fresh` orange and reducing `fresh` count by `1`.  For any *turned* oranges, their `turn_rotted` is that of the orange that turned them, plus `1`.
+4. Once all `rotten` oranges are exhausted, check the `fresh` count
+    - if `fresh > 0`, at least one fresh orange remains -> return `-1`.
+    - otherwise, return `minutes`
 
 ## Results (Python 3)
 
-**Method 1**:  ms, MB (%, %)
+**Method 1**: 88 ms, 13.9 MB (35.63%, 46.65%)
