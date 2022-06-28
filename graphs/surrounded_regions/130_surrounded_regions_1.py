dea20b569eb0f2b4e2ac838ef5037d4ca82c302a
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-06-28 04:45:31 -0700

    Add Surrounded Regions (LC130) Solution

--- graphs/surrounded_regions/130_surrounded_regions_1.py
+++ graphs/surrounded_regions/130_surrounded_regions_1.py
@@ -1,1 +1,78 @@
-#type: ignore
+from typing import List
+from collections import deque
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # Assigned land tiles by { (row, col), contiguous_group } - water is 0
+        assigned = {}
+        # Set of groups to NOT be captured - connected to border
+        non_capture = set()
+        # Current group - incremented by 1 for each new one
+        group = 0
+        # queue of (row, col) land tiles to assign to a group
+        to_assign = deque()
+        # Find all unexplored land tiles and perform BFS
+        for row, tiles in enumerate(board):
+             for col, tile in enumerate(tiles):
+                coords = (row,col)
+                if coords in assigned:
+                    continue
+                if tile == "X": 
+                    assigned[coords] = 0
+                else:
+                    # Assign new land group and find all contiguous land
+                    group += 1
+                    assigned[(row,col)] = group
+                    to_assign.append(coords)
+                    surroundedBfs(coords, board, assigned, to_assign, non_capture, group)
+
+        # Convert all land ("O") that aren't in non-caputure to water ("X")
+        for (row,col), group in assigned.items():
+            if group not in non_capture:
+                board[row][col] = "X"
+
+        return None
+
+def surroundedBfs(coords, board, assigned, to_assign, non_capture, group):
+    """BFS helper for Surrounded Regions."""  
+    # Used for bounds checks
+    last_row, last_col = len(board) - 1, len(board[0]) - 1
+    while to_assign:
+        # Get land tile and see if it's:
+        # (a) in new group (not already explored) and
+        # (b) on the edge (non-captured)
+        (row,col) = to_assign.popleft()
+        if row == 0 or row == last_row or col == 0 or col == last_col:
+            non_capture.add(group)
+        # Check for contiguous land (same group). Water is group 0
+        north = (row-1, col)
+        south = (row+1, col)
+        west  = (row, col-1)
+        east  = (row, col+1)
+        if row > 0 and north not in assigned:
+            if board[north[0]][north[1]] == "O":
+                assigned[north] = group
+                to_assign.append(north)
+            else:
+                assigned[north] = 0
+        if row < last_row and south not in assigned:
+            if board[south[0]][south[1]] == "O":
+                assigned[south] = group
+                to_assign.append(south)
+            else:
+                assigned[south] = 0
+        if col > 0 and west not in assigned:
+            if board[west[0]][west[1]] == "O":
+                assigned[west] = group
+                to_assign.append(west)
+            else:
+                assigned[west] = 0
+        if col < last_col and east not in assigned:
+            if board[east[0]][east[1]] == "O":
+                assigned[east] = group
+                to_assign.append(east)
+            else:
+                assigned[east] = 0
--- graphs/surrounded_regions/README.md
+++ graphs/surrounded_regions/README.md
@@ -16,11 +16,19 @@ Constraints:
 ## Thought Process
 
 Edge Cases / Caveats / Pitfalls:
+- Border tiles: any border tile or tile connected to one is *not captured*
+
+Big picture: a region is *not* captured if there is at least *one* tile that has neither land nor water on its NSEW edge.  In other words, a region is not captured if at least *one* tile in the region is on the border.
 
 ## Procedure
 
-### Method 1
+### Method 1: Breadth-First Search w/Non-Capture Groups
+
+Big Picture:
+1. Gather all contiguous land tiles by group (also see *Number of Islands* problem)
+2. If any member of that group is on the `board` edge, assign group to a `non_capture` set.
+3. Once all tiles explored, assign any tiles *not* in the `non_capture` set to water (`"X"`)
 
 ## Results (Python 3)
 
-**Method 1**:  ms, MB (%, %)
+**Method 1**: 322 ms, 19.5 MB (14.86%, 17.74%)
